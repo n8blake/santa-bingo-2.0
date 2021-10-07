3fb7e946c1dfca81f945625b9330d42c0f8822fd
Nate <n8blake@mac.com>
2021-10-07 16:41:55 -0700

    Update APIs to start matching SB functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,36 +2,21 @@
 import axios from "axios";
 
 export default {
-    // Get all employees
-    search: function(queryString){
-        const searchURL = 'https://www.googleapis.com/books/v1/volumes?q=' + queryString;
-        //console.log(`Getting ${queryString}`)
-        return axios.get(searchURL);
+    login: function(email){
+        console.log("Loging in with: " + email);
+        const loginURL = '/api/login/';
+        const payload = {email}
+        return axios.post(loginURL, payload);
     },
-    // Get books from our own api
-    getBooks: function(bookObj){
-        let getBooksURL = '/api/books/';
-        if(bookObj){
-            // modify url to get book url
-        }
-        return axios.get(getBooksURL);
+    validateToken: function(token){
+        console.log("validating token...");
+        const validateTokenURL = '/api/login/validate';
+        const payload = {token}
+        return axios.post(validateTokenURL, payload);
     },
-    // post a new favorite
-    postFavorite: function(bookObj){
-        console.log("posting");
-        const booksURL = '/api/books/';
-        return axios.post(booksURL, bookObj);
-    },
-    // remove a favorite
-    removeFavorite: function(id){
-        console.log("Removing");
-        const bookURL = '/api/books/' + id;
-        return axios.delete(bookURL);
-    },
-    // get a color for a book
-    getBookColor: function(url){
-        const requestBody = {url: url}
-        const colorURL = '/api/color/';
-        return axios.post(colorURL, requestBody);
+    checkLoginStatus: function(){
+        console.log("Checking login status");
+        const loginURL = '/api/login/';
+        return axios.get(loginURL);
     }
 };
