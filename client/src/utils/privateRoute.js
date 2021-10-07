91cdc7672fba9e81dc4aa5c3bcdf9d2bc741f2b6
Nate <n8blake@mac.com>
2021-10-07 16:43:51 -0700

    Define PrivateRoute wrapper

---
+++ client/src/utils/privateRoute.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useStoreContext } from "../GlobalState";
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+    const [state, dispatch] = useStoreContext();
+    
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          state.token && state.validToken ? (
+            children
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
+
+export default PrivateRoute;
