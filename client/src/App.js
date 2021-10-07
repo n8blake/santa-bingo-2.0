f70ff6981b709f46652005ef39c536ed8bbc9f35
Nate <n8blake@mac.com>
2021-10-07 16:40:04 -0700

    Basic home page and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SearchPage from "./pages/SearchPage";
-import FavoritesPage from "./pages/FavoritesPage";
-import ActiveBook from "./components/ActiveBook/ActiveBook";
+import { StoreProvider, useStoreContext } from './utils/GlobalState';
+import LoginPage from './pages/LoginPage';
+import Home from './pages/Home';
 import Header from './components/Header';
 import NoMatch from "./pages/NoMatch";
-import { StoreProvider } from './utils/GlobalState';
-
 import './App.scss';
 
 
 function App() {
   return (
     <StoreProvider>
-      <h1 className="display">SantA Bingo!</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path={"/"} children={<Home />} />
+          <Route exact path={["/login/:token", "/login"]} children={<LoginPage />} />
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
     </StoreProvider>
-    
   );
 }
 
