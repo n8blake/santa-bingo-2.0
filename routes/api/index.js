e0c9e2ee63c3d333741c4c573e2d7d2c31877b08
Nate <n8blake@mac.com>
2021-10-19 14:41:56 -0700

    Add card routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,11 @@ const router = require("express").Router();
 const loginRoutes = require("./login");
 const logoutRoute = require("./logout");
 const usersRoutes = require("./users");
-
+const cardsRoutes = require("./cards");
 // use routes
 router.use("/login", loginRoutes);
 router.use("/logout", logoutRoute);
 router.use("/users", usersRoutes);
+router.use("/cards", cardsRoutes);
 
 module.exports = router;
