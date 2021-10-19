f49e67c2b135aef9625201a2087b387a16bc0d83
Nate <n8blake@mac.com>
2021-10-19 14:32:56 -0700

    Set header back to last years SVG header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { useStoreContext } from "../../utils/GlobalState";
+import HeaderRed from '../../assets/SVG/SB_logo_red.svg';
+import HeaderGreen from '../../assets/SVG/SB_logo_green.svg';
 
 import './style.scss';
 
 function Header() {
 	const location = useLocation();
-
 	const [state, dispatch] = useStoreContext();
+	const [inGame, setInGame] = useState(false);
 
 	return(
-		<div className="jumbotron">
-			<h1 className="display hero-text">Santa Bingo</h1>
+		<div className="header">
+			<div className="jumbotron">
+				<Link to="/" style={{ textDecoration: 'none' }}>
+					{ inGame ? (
+						<img className="logoHero" src={HeaderGreen} alt="Santa Bingo"/>
+					) : (
+						<img className="logoHero" src={HeaderRed} alt="Santa Bingo"/>
+					)}
+				</Link>
+			</div>
 			<nav className="pageLinkBtn">
-			{((location.pathname.toLowerCase() ===  '/search') ||  location.pathname.toLowerCase() ===  '/' ) ? (
+			{((location.pathname.toLowerCase() ===  '/profile') ||  location.pathname.toLowerCase() ===  '/' ) ? (
 				<Link to="/profile" style={{ textDecoration: 'none' }}>
 					<i className="bi bi-person-circle"></i>
-					<div className="link-text">profile</div>
 				</Link>
 			) : (
 				<div></div>
