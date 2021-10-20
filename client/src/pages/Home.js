5f798c2f434e71f43ad9b9fbcd6f525b4219d2d4
Nate <n8blake@mac.com>
2021-10-19 17:18:54 -0700

    Add test data to home.js to work on Card component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,9 +8,18 @@ function Home(){
     
     const [state, dispatch] = useStoreContext();
 
+    const cardTitle = ['S', 'a', 'n', 't', 'A'];
+    const cells = {
+        column_0: [2, 1, 9, 11, 3],
+        column_1: [16, 17, 29, 21, 30],
+        column_2: [42, 41, 0, 31, 32],
+        column_3: [52, 51, 49, 48, 53],
+        column_4: [72, 71, 69, 65, 63],
+    }
+
     return(
         <div className="container">
-            <Card />
+            <Card title={cardTitle} cells={cells} />
         </div>
     )
 
