d0cb3779dcc51977ed974e47cb14e4443ae8f8ca
Nate <n8blake@mac.com>
2021-10-29 17:14:03 -0700

    Add CardManagerPage

---
+++ client/src/pages/CardManagerPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useContext, useCallback } from "react";
+import { useStoreContext } from "../utils/GlobalState";
+import API from "../utils/API";
+import Card from '../components/Card/Card';
+
+import '.CardManagerPage.scss';
+
+function CardManagerPage(props){
+    <div className="container">
+        Card Manager
+    </div>
+}
+
+export default CardManagerPage;
