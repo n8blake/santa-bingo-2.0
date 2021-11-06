7066b1004252d0d0bda5478ef7edae3a1889b240
Nate <n8blake@mac.com>
2021-11-05 17:45:04 -0700

    Update seedDB to have better code factorization and readability

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-const User = db.User;
+//const User = db.User;
 const Card = db.Card;
 const Game = db.Game;
 const { v4: uuidv4 } = require('uuid');
@@ -10,186 +10,52 @@ mongoose.connect(
     "mongodb://localhost/santa-bingo-n8blake"
 );
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+const createUsers = require('./CreateUsers');
+const createCards = require('./CreateCards');
 
-const USERS = [
-	  {
-          firstName: "Nate",
-          lastName: "Blake",
-          displayName: "N8",
-          email: "n8blake@mac.com",
-          color: 'red'
-      },
-      {
-        firstName: "Spencer",
-        lastName: "Blake",
-        displayName: "SB",
-        email: "spencer.blake@slcc.edu",
-        color: 'blue'
-    },
-    {
-        firstName: "Kristin",
-        lastName: "Cooper",
-        displayName: "KC",
-        email: "katiedean@mac.com",
-        color: 'yellow'
-    },
-];
+const seed = async function(){
 
-const CARDS = [];
-
-User
-    .remove({})
-    .then(() => {
-        USERS.map(user => {
-            User.create(user)
-                .then((n) => {
-                    console.log(`${n} user created`);
-                    if(USERS.indexOf(user) == USERS.length -1 ){
-                        makeCards();
-                    }   
-                    //
-                    return;
-                })
-                .catch(error => {
-                    console.log(error)
-                    process.exit(1);
-                })
-        })
-        //makeCards();
-    })
-    // .then(data => {
-    //     console.log(data.result.n + " user records insterted!");
-    //     //process.exit(0);
-    // })
-    .catch(error => {
-        console.error(error);
+    // Create New Users
+    const users = await createUsers();
+    users.length ? console.log(`${users.length} users created`) : () => {
+        console.error("No users created");
         process.exit(1);
-    });
+    };
 
-const makeCards = () => {
-    User.find({}).then(users => {
-        // Make cards for each user
-        const players = [];
-        users.map(user => {
-            players.push(user.uuid);
-        });
+    const cards = await createCards(users);
+    cards.length ? console.log(`${cards.length} cards created`) : () => {
+        console.error("No cards created");
+        process.exit(1);
+    };
+
+}
+
+seed().then(result => {
+    //console.log(result);
+    process.exit(0);
+});
+
+// Give Each user 3 - 5 cards
+// const { generateCard } = require('../utils/CardGenerator')
+// const generateCards = function(users){
     
-        Game.remove({}).then(() => { 
-            users.map((user) => {
-                const id = user.uuid;
-                makeGame(id, user.firstName, players);
-                //console.log(id);
-                for(let i = 0; i < 4; i++){
-                    const cardUUID = uuidv4();
-                    const card = {
-                        player: id,
-                        active: true,
-                        created: new Date(),
-                        uuid: cardUUID
-                    }
-                    // define col 0 'S'
-                    // 5 unique random numbers between 1 and 15
-                    card.column_0 = [];
-                    for(let j = 0; j < 5; j++){
-                        //card.column_0.push(j);
-                        let num;
-                        do {
-                            num = getRandomInt(1, 15);
-                            //console.log(num);
-                        } while(card.column_0.indexOf(num) > -1)
-                        card.column_0.push(num);
-                    }
-        
-                    // define col 1 'a'
-                    // 5 unique random numbers between 16 and 30
-                    card.column_1 = [];
-                    for(let j = 0; j < 5; j++){
-                        let num;
-                        do {
-                            num = getRandomInt(16, 30) 
-                        } while(card.column_1.indexOf(num) > -1)
-                        card.column_1.push(num);
-                    }
-        
-                    // define col 2 'n'
-                    // 5 unique random numbers between 31 and 45
-                    card.column_2 = [];
-                    for(let j = 0; j < 5; j++){
-                        let num;
-                        do {
-                            num = getRandomInt(31, 45); 
-                            //console.log(num);
-                        } while(card.column_2.indexOf(num) > -1)
-                        card.column_2.push(num);
-                        // Set index 2 to zero on every card for Col 2
-                        if(j === 2){
-                            card.column_2[j] = 0;
-                        }
-                    }
-        
-                    // define col 3 't'
-                    // 5 unique random numbers between 46 and 60
-                    card.column_3 = [];
-                    for(let j = 0; j < 5; j++){
-                        let num;
-                        do {
-                            num = getRandomInt(46, 60); 
-                        } while(card.column_3.indexOf(num) > -1)
-                        card.column_3.push(num);
-                    }
-        
-                    // define col 3 't'
-                    // 5 unique random numbers between 61 and 75
-                    card.column_4 = [];
-                    for(let j = 0; j < 5; j++){
-                        let num;
-                        do {
-                            num = getRandomInt(61, 75); 
-                        } while(card.column_4.indexOf(num) > -1)
-                        card.column_4.push(num);
-                    }
-                    //console.log(card);
-                    CARDS.push(card);
-                }
-                Card.remove({})
-                    .then(() => Card.collection.insertMany(CARDS))
-                    .then(data => {
-                        console.log(data.result.n + " card records insterted!");
-                        process.exit(0);
-                    })
-                    .catch(error => {
-                        console.error(error);
-                        process.exit(1);
-                    });
-            })
-        });
-        
-    })
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
-}
+// }
 
 
-const makeGame = (creator_uuid, name, players) => {
-    const GAME = 
-        {
-            start_time: new Date(),
-            creator: creator_uuid,
-            name: `${name}'s Game'`,
-            players: players
-        };
-    Game.create(GAME).then(() => {
-        console.log("game created");
-        
-    }).catch(error => {
-        console.log(error);
-        process.exit(1);
-    })
-}
+// Have User 1 Create a Game room
+    // 3 cards per user
+
+// Have Users 2 and 3 Join the game room
+    // select 3 random cards from each users' cards list for this game
+
+// start the game
+    // until a user gets Bingo...
+        // call number
+        // mark for each user that number being called
+        // check for bingo
+    // once a user gets bingo
+// end the game
+
+// save the game history
+
+//process.exit(0);
