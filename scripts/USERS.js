c9e0d360e64b63ccb99f903a270cfce15f332471
Nate <n8blake@mac.com>
2021-11-05 17:43:16 -0700

    Define generic users for database seed data

---
+++ scripts/USERS.js
@@ -0,0 +1,25 @@
+const USERS = [
+    {
+        firstName: "Nate",
+        lastName: "Blake",
+        displayName: "N8",
+        email: "n8blake@mac.com",
+        color: 'red'
+    },
+    {
+      firstName: "Spencer",
+      lastName: "Blake",
+      displayName: "SB",
+      email: "spencer.blake@slcc.edu",
+      color: 'blue'
+  },
+  {
+      firstName: "Kristin",
+      lastName: "Cooper",
+      displayName: "KC",
+      email: "katiedean@mac.com",
+      color: 'yellow'
+  },
+];
+
+module.exports = USERS;
