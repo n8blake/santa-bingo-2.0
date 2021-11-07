0de7afa8397e8196c750b46e05be9e59d1d9c17e
Nate <n8blake@mac.com>
2021-11-06 18:56:58 -0700

    Add GameRoom and GameType models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ module.exports = {
     Game: require("./game.js"),
     Card: require("./card.js"),
     User: require("./user.js"),
-    Game: require("./game.js")
+    GameRoom: require("./gameroom.js"),
+    GameType: require("./gametype.js")
 };
