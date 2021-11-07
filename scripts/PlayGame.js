8890a752b42d793ce86bdfcb9e8d6ec9c3e47eb3
Nate <n8blake@mac.com>
2021-11-07 14:27:37 -0800

    Update PlayGame, clear old player marks, populate player cards, call nextNumber recursively, add markCardForPlayer

--- scripts/PlayGame.js
+++ scripts/PlayGame.js
@@ -2,11 +2,16 @@ const db = require("../models");
 const Game = db.Game;
 const GameType = db.GameType;
 const Card = db.Card;
+const PlayerMark = db.PlayerMark;
 
 const randomInt = require('../utils/randomInt');
 
 module.exports = {
     clearAllGames: async function(){
+        // clear old marks
+        await PlayerMark.deleteMany({});
+        // clear old wins
+
         return Game.deleteMany({});
     },
     getPopulatedGame: async function(game) {
@@ -18,6 +23,12 @@ module.exports = {
             populate: { path: 'player',
                         select: '-email -created -__v', 
                         model: 'User' }
+        })
+        .populate({
+            path: 'players',
+            populate: { path: 'cards',
+                        select: '-created -__v', 
+                        model: 'Card' }
         })
         .populate({
             path: 'gameTypeHistory',
@@ -60,24 +71,123 @@ module.exports = {
         const _game = await Game.findOne({_id: game._id});
         // get list of numbers
         const calledNumbers = [];
+        
         _game.numbers.map(numberObject => {
             calledNumbers.push(numberObject.number);
         });
-
-        // call a random number that hasn't been called yet
-        let nextNumber = randomInt(1, 75);
-        while(calledNumbers.indexOf(nextNumber) > -1){
-            nextNumber = randomInt(1, 75);
-        }
         
+        function nextNumber() {
+            const _nextNumber = randomInt(1, 76);
+            if(calledNumbers.indexOf(_nextNumber) === -1){
+                return _nextNumber;
+            } else {
+                return nextNumber();
+            }
+        }
+        const number = nextNumber();
+
         const nextNumberObject = {
-            number: nextNumber
+            number: number
         }
 
         _game.numbers.push(nextNumberObject);
+        //return number;
+        return Game.findOneAndUpdate({_id: _game._id}, _game, {returnDocument: 'after'})
+                .then(() => {
+                    return this.getPopulatedGame(_game);
+                })
+                .catch(error => { return error });
+    },
+    markCardForPlayer: async function(game, player, card, number){
+        const playersMarks = await PlayerMark.find({player: player._id, card: card._id, game: game._id});
+        // Number is valid if: 
+        // it is not already marked on the card...
+        let markValidInPlayersMarksList = false;
+        const marks = [];
+        playersMarks.map(mark => {
+            marks.push(mark.number);
+        });
+        if(marks.indexOf(number) === -1){
+            markValidInPlayersMarksList = true;
+        }
 
-        return Game.findOneAndUpdate({_id: _game._id}, _game, {returnDocument: 'after'});
+        // AND
+        // it has been called in the game
+        // get the game in its latest state
+        const _game = await Game.findOne({_id: game._id});
+        let markValidInGameNumberList = false;
+        const gameNumbers = [];
+        _game.numbers.map(numberObject => {
+            gameNumbers.push(numberObject.number);
+        })
+        if(gameNumbers.indexOf(number) !== -1){
+            markValidInGameNumberList = true;
+        }
 
+        // AND 
+        // it appears on the card trying to be marked
+        let markValidInCardNumbersList = false;
+        if(number > 0 && number <= 15) {
+            // check column 0
+            if(card.column_0.indexOf(number) !== -1) markValidInCardNumbersList = true;
+        } else if(number >= 16 && number <= 30) {
+            // check column 1
+            if(card.column_1.indexOf(number) !== -1) markValidInCardNumbersList = true;
+        } else if(number >= 31 && number <= 45 || number === 0) {
+            // check column 2
+            if(card.column_2.indexOf(number) !== -1) markValidInCardNumbersList = true;
+        } else if(number >= 46 && number <= 60) {
+            // check column 3
+            if(card.column_3.indexOf(number) !== -1) markValidInCardNumbersList = true;
+        } else if(number >= 61 && number <= 75) {
+            // check column 4
+            if(card.column_4.indexOf(number) !== -1) markValidInCardNumbersList = true;
+        }
+
+        if(markValidInPlayersMarksList && 
+            markValidInGameNumberList && 
+            markValidInCardNumbersList ){
+            // Make new mark
+            //console.log(`valid mark: ${number} for ${player.firstName} ${player.lastName} on card: ${card._id}`)
+            const mark = {
+                player: player._id,
+                card: card._id,
+                game: game._id,
+                number: number
+            }
+            return PlayerMark.create(mark)
+            // return PlayerMark.create(mark).then(() => {
+            //         return PlayerMark.find({player: player._id, card: card._id, game: game._id})
+            //                     .populate('player')
+            //                     .populate('card')
+            //                     .populate({
+            //                         path: 'game',
+            //                         model: 'Game',
+            //                         populate: { 
+            //                             path: 'gameTypeHistory',
+            //                             populate: { path: 'gameType', 
+            //                                         model: 'GameType'}
+            //                         }
+            //                     })
+
+            //     })
+            //     .catch(error => {return error});
+        } else {
+            //console.log(`invalid mark: ${number} for ${player.firstName} ${player.lastName} on card: ${card._id}`)
+            return playersMarks;
+            // return PlayerMark.find({player: player._id, card: card._id, game: game._id})
+            //                     .populate('player')
+            //                     .populate('card')
+            //                     .populate({
+            //                         path: 'game',
+            //                         model: 'Game',
+            //                         populate: { 
+            //                             path: 'gameTypeHistory',
+            //                             populate: { path: 'gameType', 
+            //                                         model: 'GameType'}
+            //                         }
+            //                     })
+        }
     }
 
 };
