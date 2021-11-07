b9757fd9e05837462e08030eddea06bc343c0421
Nate <n8blake@mac.com>
2021-11-07 14:14:44 -0800

    Define data type for Array for better db integrity

--- models/card.js
+++ models/card.js
@@ -5,11 +5,11 @@ const { v4: uuidv4 } = require('uuid');
 
 const CardSchema = new Schema({
     player: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-    column_0: { type: Array, required: true },
-    column_1: { type: Array, required: true },
-    column_2: { type: Array, required: true },
-    column_3: { type: Array, required: true },
-    column_4: { type: Array, required: true },
+    column_0: { type: Array, of: Number, required: true },
+    column_1: { type: Array, of: Number, required: true },
+    column_2: { type: Array, of: Number, required: true },
+    column_3: { type: Array, of: Number, required: true },
+    column_4: { type: Array, of: Number, required: true },
     created: { type: Date, required: true, default: new Date() },
     active: { type: Boolean, require: true, default: true },
     uuid: { type: String, required: true, default: uuidv4, index: { unique: true }},
