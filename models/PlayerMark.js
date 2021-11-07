2246b413a0d3628e7785a5cf1adcb465bf6ab70c
Nate <n8blake@mac.com>
2021-11-07 14:15:27 -0800

    Define PlayerMark Schema

---
+++ models/PlayerMark.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+const PlayerMarkSchema = new Schema({
+    player: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    card: { type: Schema.Types.ObjectId, required: true, ref: 'Card' },
+    game: { type: Schema.Types.ObjectId, required: true, ref: 'Game' },
+    number: { type: Number, required: true },
+    timestamp: { type: Date, required: true, default: new Date()}
+});
+
+const PlayerMark = Model("PlayerMark", PlayerMarkSchema);
+
+module.exports = PlayerMark;
