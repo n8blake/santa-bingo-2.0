5c17c5d52478238d723a09493134fab05fed6104
Nate <n8blake@mac.com>
2021-11-13 12:52:50 -0800

    Define win condtions in GameTypeSchema

--- models/gametype.js
+++ models/gametype.js
@@ -4,6 +4,11 @@ const Model = mongoose.model;
 
 const GameTypeSchema = new Schema({
     type: { type: String, required: true, default: 'bingo'},
+    // define win conditions
+    columns: {type: Array, of: Number },
+    rows: {type: Array, of: Number },
+    diagonals: {type: Array, of: Number },
+    operator: {type: String, required: true}
 });
 
 const GameType = Model("GameType", GameTypeSchema);
