3d9673add4c83e75ea1dd0d18eca85889ceada0a
Nate <n8blake@mac.com>
2021-11-13 12:53:52 -0800

    Define Win model

---
+++ models/Win.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+const winDataIndexSubschema = {
+    '0': {type: Array, of: Number },
+    '1': {type: Array, of: Number },
+    '2': {type: Array, of: Number },
+    '3': {type: Array, of: Number },
+    '4': {type: Array, of: Number },
+}
+
+const winDataSubschema = {
+    columns: {type: winDataIndexSubschema },
+    rows: {type: winDataIndexSubschema },
+    diagonals: {type: winDataIndexSubschema },
+}
+
+const WinSchema = new Schema({
+    player: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    card: { type: Schema.Types.ObjectId, required: true, ref: 'Card' },
+    marks: [{ type: Schema.Types.ObjectId, required: true, ref: 'PlayerMark' }], 
+    game: { type: Schema.Types.ObjectId, required: true, ref: 'Game' },
+    timestamp: { type: Date, required: true, default: new Date()}
+});
+
+const Win = Model("Win", WinSchema);
+
+module.exports = Win;
