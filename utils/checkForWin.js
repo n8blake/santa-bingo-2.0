818d7715266a3ce4380a61f05c098b7d8189f7ac
Nate <n8blake@mac.com>
2021-11-13 12:56:03 -0800

    still work in progress. Will update later

--- utils/checkForWin.js
+++ utils/checkForWin.js
@@ -7,6 +7,13 @@ const gameTypes = {
     BLACKOUT: 'blackout'
 }
 
+const compare =  (op, arg1, arg2) => {
+    if(op === 'OR'){
+        return arg1 || arg2;
+    } else {
+        return arg1 && arg2;
+    }
+}
 // Provided a GameType, a Card, and 
 // an array of PlayerMarks, check to 
 // see if there is a win.
@@ -17,13 +24,12 @@ const checkForWin = (gameType, card, marks) => {
         type: gameType,
         playerMarks: marks
     }
-    const marksNumbers = [];
-    marks.map(mark => {
+    const marksNumbers = marks.map(mark => {
         if(!isEqual(card._id, mark.card._id)){
             // throw error?
             throw new Error('PlayerMark Card mismatch.');
         }
-        marksNumbers.push(mark.number);
+        return mark.number;
     })
 
     const columnFills = [true, true, true, true, true];
@@ -46,44 +52,94 @@ const checkForWin = (gameType, card, marks) => {
         });
     }
 
-    check.columns = columnFills;
-    check.rows = rowFills;
-    check.diagonals = diagonalFills;
-
-    switch (gameType){
-        case gameTypes.BINGO:
-            // bingo logic
-            // For bingo, a win is when a player has
-            // marked off any full column of a card,
-            // any full row of a card, or any of the 
-            // two diagonals of the card
-            return {
-                ...check,
-                win: ( check.columns.indexOf(true) > -1 || 
-                        check.rows.indexOf(true) > -1 || 
-                        check.diagonals.indexOf(true) > -1 )
-            };
-        case gameTypes.X:
-            // X logic
-            return {
-                ...check,
-                win: (check.diagonals[0] && check.diagonals[1])
-            };
-        case gameTypes.WINDOW:
-            // Window logic
-            return {
-                ...check,
-                win: (check.row[0] && check.row[4] &&
-                    check.columns[0] && check.columns[4])
-            };
-        case gameTypes.BLACKOUT:
-            // Blackout logic
-            return {
-                ...check,
-                win: ( win.columns.indexOf(false) === -1 )
-            };
+    const recursiveOR = function(test, arr){
+        if(test){
+            return test;
+        } else if(arr.length === 0){
+            return test;
+        } else {
+            const newTest = arr.pop();
+            return recursiveOR(newTest, arr);
+        }
     }
-    return check;
+
+    const recursiveAND = function(test, arr){
+        if(!test){
+            return test;
+        } else if(arr.length === 0){
+            return test;
+        } else {
+            const newTest = arr.pop();
+            return recursiveAND(newTest, arr);
+        }
+    }
+
+    const cols = columnFills.map(v => {
+        if(gameType.columns.indexOf(columnFills.indexOf(v)) > -1){
+            return v
+        }  
+    });
+    const rows = rowFills.map(v => {
+        if(gameType.rows.indexOf(rowFills.indexOf(v)) > -1){
+            return v
+        }
+    });
+    const diags = diagonalFills.map(v => {
+        if(gameType.diagonals.indexOf(diagonalFills.indexOf(v)) > -1){
+            return v
+        }  
+    });
+
+    let colWin;
+    let rowWin;
+    let diagsWin;
+
+    let win;
+
+    if(gameType.operator === 'OR'){
+        colWin = recursiveOR(cols.pop(), cols);
+        if(colWin){
+            // put the marks in the check columns
+            check.columns = columnFills;
+        }
+        rowWin = recursiveOR(rows.pop(), rows);
+        if(rowWin){
+            check.rows = rowFills;
+        }
+        diagsWin = recursiveOR(diags.pop(), diags);
+        if(diagsWin){
+            check.diagonals = diagonalFills;
+        }
+        win = (colWin || rowWin || diagsWin);
+    } else {
+        win = true;
+        if(gameType.columns.length > 0){
+            colWin = recursiveAND(cols.pop(), cols);
+            win = win && colWin;
+        }
+        if(gameType.rows.length > 0){
+            rowWin = recursiveAND(rows.pop(), rows);
+            win = win && rowWin;
+        }
+        if(gameType.diagonals.length > 0){
+            diagsWin = recursiveAND(diags.pop(), diags);
+            win = win && diagsWin;
+        }
+        if(win && colWin){
+            check.columns = columnFills;
+        }
+        if(win && rowWin){
+            check.rows = rowFills;
+        }
+        if(win && diagsWin){
+            check.diagonals = diagonalFills;
+        }
+    }
+
+    return {
+        ...check, 
+        win: win
+    }
 }
 
 module.exports = checkForWin;
