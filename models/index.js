33c34fe5e5408a4800a395d4a7e839fa354e5ace
Nate <n8blake@mac.com>
2021-11-13 12:53:09 -0800

    Export new models

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 module.exports = {
     Game: require("./Game.js"),
     Card: require("./Card.js"),
-    User: require("./user.js"),
+    User: require("./User.js"),
     GameRoom: require("./GameRoom.js"),
     GameType: require("./GameType.js"),
-    PlayerMark: require("./PlayerMark.js")
+    PlayerMark: require("./PlayerMark.js"),
+    Win: require("./Win.js")
 };
