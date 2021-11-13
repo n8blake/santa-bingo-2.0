5dac3535740f8d6741c93a131e5d58a5c6943e6c
Nate <n8blake@mac.com>
2021-11-13 12:54:53 -0800

    Seed new default gameTypes using updated data structure

--- scripts/CreateGameTypes.js
+++ scripts/CreateGameTypes.js
@@ -3,10 +3,40 @@ const { GameType } = require("../models");
 const createGameTypes = async function() {
     return GameType.deleteMany({})
         .then(() => {
-            const gameTypes = ['bingo', 'x', 'window', 'blackout'];
+            //const gameTypes = ['bingo', 'x', 'window', 'blackout'];
+            const gameTypes = [
+                { 
+                    type: 'bingo',
+                    columns: [0, 1, 2, 3, 4],
+                    rows: [0, 1, 2, 3, 4],
+                    diagonals: [0, 1], 
+                    operator: 'OR'
+                },
+                { 
+                    type: 'x',
+                    columns: [],
+                    rows: [],
+                    diagonals: [0, 1], 
+                    operator: 'AND'
+                },
+                { 
+                    type: 'window',
+                    columns: [0, 4],
+                    rows: [0, 4],
+                    diagonals: [], 
+                    operator: 'AND'
+                },
+                { 
+                    type: 'blackout',
+                    columns: [0, 1, 2, 3, 4],
+                    rows: [0, 1, 2, 3, 4],
+                    diagonals: [0, 1], 
+                    operator: 'AND'
+                },
+            ]
             const gameTypesPromises = [];
             for(let i = 0; i < gameTypes.length; i++){
-                const gameTypePromise = GameType.create({type: gameTypes[i]});
+                const gameTypePromise = GameType.create(gameTypes[i]);
                 gameTypesPromises.push(gameTypePromise);
             }
             return Promise.all(gameTypesPromises);
