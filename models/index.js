71ce02f43b4e55547c912df87d8b957b9c707795
Nate <n8blake@mac.com>
2021-11-13 16:08:27 -0800

    update model names for heroku

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-    Game: require("./Game.js"),
-    Card: require("./Card.js"),
-    User: require("./User.js"),
-    GameRoom: require("./GameRoom.js"),
-    GameType: require("./GameType.js"),
-    PlayerMark: require("./PlayerMark.js"),
+    Game: require("./game.js"),
+    Card: require("./card.js"),
+    User: require("./user.js"),
+    GameRoom: require("./gameroom.js"),
+    GameType: require("./gametype.js"),
+    PlayerMark: require("./playermark.js"),
     Win: require("./Win.js")
 };
