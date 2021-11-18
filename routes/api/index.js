e93b957b5bcf5e83f69674b8265317411a05cc6d
Nate <n8blake@mac.com>
2021-11-17 19:01:13 -0800

    Add passport to protect login-restriced routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,8 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const passport = require('passport');
+
+const withAuth = passport.authenticate('jwt', { session: true, failureFlash: 'Invalid username or password.' });
+
 // require routes files
 const loginRoutes = require("./login");
 const logoutRoute = require("./logout");
@@ -9,8 +13,10 @@ const gameRoutes = require('./game');
 // use routes
 router.use("/login", loginRoutes);
 router.use("/logout", logoutRoute);
+//passport.authenticate('local', { failureRedirect: '/login', flashFailure: true }),
+router.use("/users", withAuth);
 router.use("/users", usersRoutes);
-router.use("/cards", cardsRoutes);
+router.use("/cards", withAuth, cardsRoutes);
 router.use("/game", gameRoutes);
 
 module.exports = router;
