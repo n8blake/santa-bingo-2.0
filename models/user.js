cd00b06693f21d8453291492abfee6990408ea5c
Nate <n8blake@mac.com>
2021-11-17 18:29:59 -0800

    Use bcrypt for password hashing and add validatePassword instance method to UserSchema

--- models/user.js
+++ models/user.js
@@ -2,30 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Model = mongoose.model;
 const { v4: uuidv4 } = require('uuid');
-
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     firstName: String,
     lastName: String,
     color: { type: String, default: 'red'},
-    //password: { type: String },
+    password: { type: String, required: true },
     displayName: { type: String, required: true },
     email: { type: String, required: true, index: { unique: true } },
     created: { type: Date, required: true, default: new Date() },
     uuid: { type: String, required: true, default: uuidv4, index: { unique: true }},
 });
 
-// UserSchema.pre("save", function(next){
-//     if(!this.isModified("password")) return next();
-//     bcrypt.hash(this.password, 16.5, (err, hash) => {
-//         if(err) {
-//             next(err);
-//             return;
-//         }
-//         this.password = hash;
-//         next();
-//     })
-// });
+UserSchema.pre("save", function(next){
+    if(!this.isModified("password")) return next();
+    bcrypt.hash(this.password, 16.5, (err, hash) => {
+        if(err) {
+            next(err);
+            return;
+        }
+        this.password = hash;
+        next();
+    })
+});
+
+UserSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 
 const User = Model("User", UserSchema);
 
