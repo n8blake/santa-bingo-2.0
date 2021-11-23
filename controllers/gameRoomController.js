54642f5073ae6f63f97b4f6259add2318ab9e282
Nate <n8blake@mac.com>
2021-11-22 19:30:39 -0800

    Populate settings for gamerooms. Add update method

--- controllers/gameRoomController.js
+++ controllers/gameRoomController.js
@@ -1,8 +1,9 @@
-const { GameRoom } = require('../models');
+const { GameRoom, GameSettings, GameType } = require('../models');
+const isEqual = require('lodash.isequal');
 
 module.exports = {
     findAll: function(request, response){
-        GameRoom.find({})
+        GameRoom.find({public: true})
             .populate({
                 path: 'creator',
                 select: '-email -password -created -__v', 
@@ -14,8 +15,36 @@ module.exports = {
                             select: '-email -created -__v', 
                             model: 'User' }
             })
+            .populate({
+                path:'settings',
+                populate: [{
+                    path: 'gameManagers',
+                    populate: {
+                        path: 'manager',
+                        select: '-email -created -__v', 
+                        model: 'User' 
+                    }
+                },
+                {
+                    path: 'prizeManagers',
+                    populate: {
+                        path: 'manager',
+                        select: '-email -created -__v', 
+                        model: 'User' 
+                    }
+                },
+                {
+                    path: 'gameTypeHistory',
+                    populate: {
+                        path: 'gameType',
+                        select: 'type',
+                        model: 'GameType'
+                    }
+                }
+            ]
+            })
             .then(rooms => {
-                if(rooms){
+                if(rooms && rooms.length > 0){
                     response.json(rooms);
                 } else {
                     response.status(404).send("No rooms found");
@@ -27,6 +56,45 @@ module.exports = {
     },
     findById: function(request, response){
         GameRoom.findOne({_id: request.params.id})
+            .populate({
+                path: 'creator',
+                select: '-email -password -created -__v', 
+                model: 'User'
+            })
+            .populate({
+                path: 'players',
+                populate: { path: 'player',
+                            select: '-email -created -__v', 
+                            model: 'User' }
+            })
+            .populate({
+                path:'settings',
+                populate: [{
+                    path: 'gameManagers',
+                    populate: {
+                        path: 'manager',
+                        select: '-email -created -__v', 
+                        model: 'User' 
+                    }
+                },
+                {
+                    path: 'prizeManagers',
+                    populate: {
+                        path: 'manager',
+                        select: '-email -created -__v', 
+                        model: 'User' 
+                    }
+                },
+                {
+                    path: 'gameTypeHistory',
+                    populate: {
+                        path: 'gameType',
+                        select: 'type',
+                        model: 'GameType'
+                    }
+                }
+            ]
+            })
             .then(room => {
                 if(room){
                     response.json(room);
@@ -38,10 +106,28 @@ module.exports = {
                 response.status(422).json(error);
             })
     },
-    create: function(request, response){
+    create: async function(request, response){
+        request.body.creator = request.user._id;
         if(request.user.role !== 'admin' || typeof request.body.creator === undefined){
             request.body.creator = request.user._id;
         }
+
+        const bingoGameType = await GameType.findOne({type: 'bingo'});
+
+        // set gameroom settings
+        const defaultGameSettings = {
+            gameManagers: [{manager: request.user._id}],
+            prizeManagers: [{manager: request.user._id}],
+            gameTypeHistory: [{gameType: bingoGameType._id}]
+        }
+
+        const settings = await GameSettings.create(defaultGameSettings);
+        if(settings._id){
+            request.body.settings = settings._id;
+        } else {
+            reponse.status(400).send('default game settings not set');
+        }
+
         GameRoom.create(request.body)
             .then(dbModel => {
                 response.json(dbModel);
@@ -51,9 +137,18 @@ module.exports = {
             })
     },
     update: function(request, response){
-        // if(request.body.creator !== request.user._id || request.user.role !== 'admin'){
-        //     response.status(403).send("Cannot create games on behalf of other users")
-        // }
-        response.status(404).send("route not yet supported")
+        GameRoom.findOne({_id: request.params.id}).then(gameroom => {
+            if(gameroom){
+                if(request.user.role === 'admin' || isEqual(gameroom.creator, request.user._id)){
+                    GameRoom.updateOne({_id: request.params.id}, request.body)
+                        .then(dbResult => response.json(dbResult))
+                        .catch(error => response.status(422).json(error));
+                } else {
+                    response.status(403).send('You are not permitted to modify this resource.');
+                }
+            } else {   
+                response.status(404).send("gameroom not found")
+            }
+        })
     }
 }
