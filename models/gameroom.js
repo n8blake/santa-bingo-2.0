79db391a90c6c9f32742d19c16af6518e69567cf
Nate <n8blake@mac.com>
2021-11-22 17:03:08 -0800

    use gameSettings model in gameRoom

--- models/gameroom.js
+++ models/gameroom.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Model = mongoose.model;
-//const Game = require("./game");
-const { v4: uuidv4 } = require('uuid');
 
 const GameRoomSchema = new Schema({
     inGame: { type: Boolean, required: true, default: false},
-    uuid: { type: String, required: true, default: uuidv4 },
     creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     roomName: { type: String, required: true },
     code: { type: String },
-    gameManagers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    prizeManagers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    active: { type: Boolean, required: true, default: true },
+    public: { type: Boolean, required: true, default: true },
+    settings: { type: Schema.Types.ObjectId, required: true, ref: 'GameSettings'},
     players: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     games: [{type: Schema.Types.ObjectId, ref: 'Game'}]
 });
