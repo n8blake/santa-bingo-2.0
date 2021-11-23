2266a92dbd139318e420f880cc0afb474224da59
Nate <n8blake@mac.com>
2021-11-22 17:06:50 -0800

    Populate GameRoom return objects. Update single find method to findById

--- controllers/gameRoomController.js
+++ controllers/gameRoomController.js
@@ -1,33 +1,59 @@
-const { GameRoom, User } = require('../models');
+const { GameRoom } = require('../models');
 
 module.exports = {
     findAll: function(request, response){
-        if(!request.session.token) response.status(401).send();
         GameRoom.find({})
-            .then(dbModel => {
-                response.json(dbModel);
+            .populate({
+                path: 'creator',
+                select: '-email -password -created -__v', 
+                model: 'User'
+            })
+            .populate({
+                path: 'players',
+                populate: { path: 'player',
+                            select: '-email -created -__v', 
+                            model: 'User' }
+            })
+            .then(rooms => {
+                if(rooms){
+                    response.json(rooms);
+                } else {
+                    response.status(404).send("No rooms found");
+                }
             })
             .catch(error => {
                 response.status(422).json(error);
             })
     },
-    find: function(request, response){
-        if(!request.session.token) response.status(401).send();
-        GameRoom.findOne({uuid: request.params.id})
-            .then(dbModel => {
-                response.json(dbModel);
+    findById: function(request, response){
+        GameRoom.findOne({_id: request.params.id})
+            .then(room => {
+                if(room){
+                    response.json(room);
+                } else {
+                    response.status(404).send("No room found");
+                }
             })
             .catch(error => {
                 response.status(422).json(error);
             })
     },
     create: function(request, response){
+        if(request.user.role !== 'admin' || typeof request.body.creator === undefined){
+            request.body.creator = request.user._id;
+        }
         GameRoom.create(request.body)
-        .then(dbModel => {
-            response.json(dbModel);
-        })
-        .catch(error => {
-            response.status(422).json(error);
-        })
+            .then(dbModel => {
+                response.json(dbModel);
+            })
+            .catch(error => {
+                response.status(422).json(error);
+            })
+    },
+    update: function(request, response){
+        // if(request.body.creator !== request.user._id || request.user.role !== 'admin'){
+        //     response.status(403).send("Cannot create games on behalf of other users")
+        // }
+        response.status(404).send("route not yet supported")
     }
 }
