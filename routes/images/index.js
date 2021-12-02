6e65c3f2680f0c2c6d507f3cd7362387dd8c8518
Nate <n8blake@mac.com>
2021-12-01 17:25:10 -0800

    Dont log every time an icon is served

--- routes/images/index.js
+++ routes/images/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const path = require("path");
 
 router.use("/santa/:icon", function(req, res) {
-    console.log("serving icon: " + req.params.icon)
+    //console.log("serving icon: " + req.params.icon)
     res.sendFile(path.join(__dirname, `../../client/src/assets/SVG/${req.params.icon}`));
 });
 
