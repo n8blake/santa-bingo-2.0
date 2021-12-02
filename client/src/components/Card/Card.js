449b2bbb4d5f7970edc1020e026d645001dd92f7
Nate <n8blake@mac.com>
2021-12-01 17:26:12 -0800

    Update card component to work with MiniCardViewer

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,9 +4,9 @@ import './Card.scss'
 function Card(props) {
 
     const [interactable, setInteractable] = useState(false);
-
+    const [thumbnail, setThumbnail] = useState(false);
     const tableHead = props.title.map((letter) => 
-        <th className="bingoCardHeader" key={letter}>{letter}</th>
+        <th className={`bingoCardHeader ${thumbnail ? "bingo-header-thumb" : ""}`} key={letter}>{letter}</th>
     );
 
     function cellInteractableDynamicClass() {
@@ -21,19 +21,19 @@ function Card(props) {
     const cells = rows.map((row) => {
         return (<tr className="bingoCardRow" key={row}>
             <td className={`bingoCell ${interactable ? "bingoCellInteractable" : ""}`}>
-                <img src={`/images/santa/${props.data.column_0[row]}.svg`} alt="" />
+                <img className={`${thumbnail ? "bingo-img-thumb" : ""}`} src={`/images/santa/${props.data.column_0[row]}.svg`} alt="" />
             </td>
             <td className={`bingoCell ${interactable ? "bingoCellInteractable" : ""}`}>
-                <img src={`/images/santa/${props.data.column_1[row]}.svg`} alt="" />
+                <img className={`${thumbnail ? "bingo-img-thumb" : ""}`} src={`/images/santa/${props.data.column_1[row]}.svg`} alt="" />
             </td>
             <td className={`bingoCell ${interactable ? "bingoCellInteractable" : ""}`}>
-                <img src={`/images/santa/${props.data.column_2[row]}.svg`} alt="" />
+                <img className={`${thumbnail ? "bingo-img-thumb" : ""}`} src={`/images/santa/${props.data.column_2[row]}.svg`} alt="" />
             </td>
             <td className={`bingoCell ${interactable ? "bingoCellInteractable" : ""}`}>
-                <img src={`/images/santa/${props.data.column_3[row]}.svg`} alt="" />
+                <img className={`${thumbnail ? "bingo-img-thumb" : ""}`} src={`/images/santa/${props.data.column_3[row]}.svg`} alt="" />
             </td>
             <td className={`bingoCell ${interactable ? "bingoCellInteractable" : ""}`}>
-                <img src={`/images/santa/${props.data.column_4[row]}.svg`} alt="" />
+                <img className={`${thumbnail ? "bingo-img-thumb" : ""}`} src={`/images/santa/${props.data.column_4[row]}.svg`} alt="" />
             </td>
         </tr>)
     });
@@ -42,10 +42,13 @@ function Card(props) {
         if(props.interactable){
             setInteractable(true);
         }
+        if(props.thumbnail){
+            setThumbnail(true);
+        }
     }, [props])
 
     return(
-        <div className="bingoCardContainer" onClick={props.clickHandler}>
+        <div className={`bingoCardContainer ${thumbnail ? "bingo-card-thumb" : ""}`} onClick={props.clickHandler}>
             <table className="bingoCard">
                 <thead>
                     <tr >
